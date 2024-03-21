dad353c7dd71070903b9ab3beb707fedc98c2b52
Daria Bajda <daria.bajda@gmail.com>
2024-03-21 18:03:19 +0100

    Split CI workflows

--- tests/ui/smoke.spec.ts
+++ tests/ui/smoke.spec.ts
@@ -1,13 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
+test('UI smoke test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
